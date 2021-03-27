cdb4dca6759720478f0eeafd5732e711293ce6ca
Peter Connor <peter.t.connor@gmail.com>
2021-03-26 20:16:41 -0400

    Initial Commit

---
+++ Shared/AccelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelApp.swift
+//  Shared
+//
+//  Created by Pete Connor on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AccelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
